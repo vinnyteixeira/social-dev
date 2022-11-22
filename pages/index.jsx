376fe67d7e495fb400a25870743be1f8e8a01982
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-22 13:42:22 -0300

    Criado componente de post

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,32 @@ import Navbar from "../src/components/layout/Navbar";
 import styled from "styled-components";
 import Container from "../src/components/layout/Container";
 import CreatePost from "../src/components/cards/CreatePost";
+import H3 from "../src/components/typography/H3";
+import Post from "../src/components/cards/Post";
 
 const Content = styled.div`
   margin: 50px 0;
+`;
+
+const LastPostText = styled.div`
+  padding: 40px 0;
+`;
+
+const RefreshPosts = styled.span`
+  font-weight: bold;
+  color: ${(props) => props.theme.primary};
+  cursor: pointer;
+`;
+
+const RefreshPostsContainer = styled.div`
+  text-align: center;
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
 `;
 
 function HomePage() {
@@ -14,6 +37,17 @@ function HomePage() {
       <Content>
         <Container>
           <CreatePost />
+          <LastPostText>
+            <H3>Últimas postagens:</H3>
+          </LastPostText>
+          <RefreshPostsContainer>
+            <RefreshPosts>Carregar novas postagens</RefreshPosts>
+          </RefreshPostsContainer>
+          <PostContainer>
+            <Post />
+            <Post />
+            <Post />
+          </PostContainer>
         </Container>
       </Content>
     </>
