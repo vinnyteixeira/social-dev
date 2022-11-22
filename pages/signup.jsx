6092320e133c9db3dc9224d3ee55c0e2926d1ae9
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-22 12:14:50 -0300

    Criado signup e utilizando virtual DOM

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -23,24 +23,27 @@ const Text = styled.p`
   text-align: center;
 `;
 
-function LoginPage() {
+function SignupPage() {
   return (
     <ImageWithSpace>
       <H1># Social Dev</H1>
       <H4>Tudo que acontece no mundo dev, está aqui!</H4>
       <FormContainer>
-        <H2>Entre em sua conta</H2>
+        <H2>Crie sua conta</H2>
         <Form>
+          <Input label="Nome" />
+          <Input label="Sobrenome" />
+          <Input label="Usuário" />
           <Input label="Email ou usuário" type="email" />
           <Input label="Senha" type="password" />
           <Button>Entrar</Button>
         </Form>
         <Text>
-          Não possui uma conta?<Link href="/signup"> Faça seu cadastro</Link>
+          Já possui uma conta?<Link href="/login"> Faça seu login</Link>
         </Text>
       </FormContainer>
     </ImageWithSpace>
   );
 }
 
-export default LoginPage;
+export default SignupPage;
