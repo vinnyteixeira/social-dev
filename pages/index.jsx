7c2af4b6e1efcb5ddf0ef64dcf40dcbab10c16a6
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-22 12:25:58 -0300

    criado Navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
-function HomePage () {
-  
+import Navbar from "../src/components/layout/Navbar";
+
+function HomePage() {
   return (
-    <div>
-      SejaDev
-    </div>
-  )
+    <>
+      <Navbar />
+      <div>SejaDev</div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
